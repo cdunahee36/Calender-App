adb7f3fc09d961e5196e9b2c3699f58c2cfc4892
cdunahee36 <dunahee10@gmail.com>
2020-10-27 12:10:15 -0500

    Added the date to the top of page.

--- script.js
+++
@@ -1,7 +0,0 @@
-//Create a header with the current date.
-//Create time blocks that have the business hours displayed in them.
-//Create a function that makes the time blocks change color if in the past present or future.
-//Create a input field for each time block.
-//Create a save button on each time block that saves the user input to localStorage
-
-
